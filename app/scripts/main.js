d3dd68953369f958fa411b404336b53ccc88609c
Alexey <lxmlshv97@mail.ru>
2022-07-12 00:44:46 +0300

    new settings

---
+++ app/scripts/main.js
@@ -0,0 +1,4 @@
+// import 'fslightbox'; // Lightbox: npm install fslightbox, site: https://fslightbox.com/javascript/documentation
+// import Swiper from 'swiper'; // Slider: npm install swiper, site: https://swiperjs.com/get-started
+// import AirDatepicker from 'air-datepicker'; // Datepicker: npm i air-datepicker -S, site: https://air-datepicker.com/ru
+
