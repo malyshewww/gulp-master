9d3aa58211ca06c7f15d805fc86b274adf39556e
malyshewww <105677749+malyshewww@users.noreply.github.com>
2024-03-16 18:26:58 +0300

    change main from browser

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 // import 'fslightbox'; // Lightbox: npm install fslightbox, site: https://fslightbox.com/javascript/documentation
 // import Swiper from 'swiper'; // Slider: npm install swiper, site: https://swiperjs.com/get-started
 // import AirDatepicker from 'air-datepicker'; // Datepicker: npm i air-datepicker -S, site: https://air-datepicker.com/ru
-console.log("main init");
+console.log("main initialize");
 
 let date = new Date().getFullYear();
 document.getElementById("year").innerHTML = date;
